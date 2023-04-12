22938b2d16bca48f3e77959dc2f4f70a78b6c3d8
kaikyalvaro1708 <kaikialvaro57@gmail.com>
2023-04-11 21:01:56 -0300

    Iniciando funções

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,21 @@
-// Sem comentários
+// Funções
+
+const divElement = document.getElementById("div-box")
+
+// divElement.addEventListener("click", ()=>{
+//     console.log(divElement);
+// });
+
+divElement.addEventListener("click", mostraElemento);
+
+function mostraElemento(){
+    console.log(divElement)
+}
+
+function soma(){
+    let n1 = 10
+    let n2 =20
+    console.log(n1+n2)
+}
+
+
